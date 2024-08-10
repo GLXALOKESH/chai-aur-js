0298ef3e9261f7af223727f32bac1b1ffcbf0acf
GLXALOKESH <alokeshmaitra9@gmail.com>
2024-08-10 12:04:30 +0530

    completed until video 12

---
+++ vid11-string/string.js
@@ -0,0 +1,32 @@
+let test = "Alokesh"
+
+console.log(test[0]);
+console.log(test.length);
+
+console.log(test.toUpperCase());
+
+console.log(test.toLowerCase());
+
+console.log(test.charAt(2));
+
+console.log(test.indexOf("o"));
+
+console.log(test.substring(3,7));
+
+console.log(test.slice(-6,2));
+
+console.log(test.trim());
+console.log(test.replace("ke", "alokesh"));
+console.log(test.includes("h"));
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ vid12-numbers_and_maths/numbers_and_Math.js
@@ -0,0 +1,40 @@
+let numbers = new Number(151)
+
+console.log(numbers);
+
+console.log(numbers.toString());
+
+let myNumber = 112.124
+
+console.log(myNumber.toFixed(2));
+console.log(myNumber.toPrecision(3));
+
+let myNumber2 = 1000000
+
+console.log(myNumber2.toLocaleString());
+console.log(myNumber2.toLocaleString('en-IN'));
+
+console.log(Number.MIN_VALUE);
+console.log(Number.MAX_VALUE);
+console.log(Number.MAX_SAFE_INTEGER);
+
+console.log(Math.abs(-22));
+console.log(Math.round(1.224));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(2.8));
+console.log(Math.random());
+let max = 20 
+let min = 10
+console.log(Math.floor(Math.random() * (max-min+1)+min));
+
+
+
+
+
+
+
+
+
+
+
+
